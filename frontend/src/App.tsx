0852386d32dc10b6354df7ad7ac6d2318676c412
PauloAndersonN <pardoansu@gmail.com>
2022-01-15 21:54:26 -0300

    pequenos ajustes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,12 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Navbar from 'componenets/Navbar';
+// import Navbar from 'componenets/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
