18d3dd94af7c6bfddfe66dd4180cb1a09331eb6d
PauloAndersonN <pardoansu@gmail.com>
2022-01-16 18:27:21 -0300

    Show score

--- frontend/src/componenets/MovieScore/index.tsx
+++ frontend/src/componenets/MovieScore/index.tsx
@@ -1,14 +1,17 @@
 import MovieStars from "componenets/MovieStars";
 import './style.css';
 
-const score = 13;
-const count = 3 ;
+type Props = {
+    score: number;
+    count: number;
+}
 
-    function MovieScore(){
+
+    function MovieScore({score,count}:Props){
         return (
             <div className="dsmovie-score-container">
         <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-        <MovieStars />
+        <MovieStars score={score}/>
         <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
         )
